e84dd09de4cf7fd0c5b736d9253b6291316b1154
Roman-Bieloshchuk <beloshchuk@gmail.com>
2024-08-24 17:00:20 +0300

    homework-1.2

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,10 @@ function getElementWidth(content, padding, border) {
     const numContent = Number.parseFloat(content);
     const numPadding = Number.parseFloat(padding);
     const numBorder = Number.parseFloat(border);
-    getElementWidth = numContent + (numPadding + numBorder) * 2;
-    return getElementWidth;
+    numGetElementWidth = numContent + (numPadding + numBorder) * 2;    
+    return numGetElementWidth;
 }
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
 
